e827ff845a603a5dc40107aa8281d407152afda1
ATHUL ANAND <athulanand@ATHULs-MacBook-Pro.local>
2019-07-02 23:09:41 +0530

    Exploratory plot 1 - Global_active_power histogram

---
+++ plot1.R
@@ -0,0 +1,21 @@
+# Plot 1
+
+
+# Loading Data
+path = "/Users/athulanand/Documents/household_power_consumption.txt"
+my_data <- read.table(path, sep = ";", h=T, colClasses = c("character", "character", rep("numeric",7)), na.strings = "?")
+# Creating DateTime column
+my_data$DateTime <- strptime(paste(my_data$Date, my_data$Time), "%d/%m/%Y %H:%M:%S")
+
+# Subseting Data based on criteria
+
+subset_data = subset(my_data, as.Date(DateTime) >= as.Date("2007-02-01") & as.Date(DateTime) <= as.Date("2007-02-02"))
+
+
+# Plotting
+
+png("plot1.png", height=480, width=480)
+
+hist(subset_data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+dev.off()
+
