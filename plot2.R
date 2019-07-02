4fcd321e21455c0f719ea38d65a3385da658ef3b
ATHUL ANAND <athulanand@ATHULs-MacBook-Pro.local>
2019-07-02 23:27:34 +0530

    Exploratory plot 2 - Global_active_power vs DateTime

--- plot2.R
+++ plot2.R
@@ -3,6 +3,7 @@
 
 # Loading Data
 path = "household_power_consumption.txt"
+
 my_data <- read.table(path, sep = ";", h=T, colClasses = c("character", "character", rep("numeric",7)), na.strings = "?")
 # Creating DateTime column
 my_data$DateTime <- strptime(paste(my_data$Date, my_data$Time), "%d/%m/%Y %H:%M:%S")
@@ -14,8 +15,9 @@ subset_data = subset(my_data, as.Date(DateTime) >= as.Date("2007-02-01") & as.Da
 
 # Plotting
 
-png("plot1.png", height=480, width=480)
+png("plot2.png", height=480, width=480)
 
-hist(subset_data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+plot(subset_data$DateTime, subset_data$Global_active_power, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
+lines(subset_data$DateTime, subset_data$Global_active_power)
 dev.off()
 
